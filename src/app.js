963f04b41c3824b5e7ca3fac509a9117e612ce69
Cherry Ng <cherry92@gmail.com>
2016-09-22 21:19:28 +0800

    Fix image pyramid code

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('it sucks')
+import pyramid from './pyramid'
+
+const sourceImage = document.getElementById('source')
+sourceImage.addEventListener('load', function () {
+
+  const layers = pyramid(sourceImage, 7)
+  console.log(layers)
+
+})
--- src/pyramid.js
+++ src/pyramid.js
@@ -1,26 +1,30 @@
+import readPixels from './read-pixels'
+
 const pyramid = function (image, numLayers) {
   const canvas = document.createElement('canvas')
   const context = canvas.getContext('2d')
 
   canvas.width = image.width
   canvas.height = image.height
-  context.drawImage(image, 0, 0, image.width. image.height)
+  context.drawImage(image, 0, 0, image.width, image.height)
 
   let layers = new Array(numLayers)
   let width = image.width
   let height = image.height
+  let ratio = 1
   for (let i = 0; i < numLayers; i++) {
     const roundWidth = Math.round(width)
     const roundHeight = Math.round(height)
 
     context.drawImage(canvas, 0, 0, roundWidth, roundHeight)
-    layers[i] = {
-      width: roundWidth, height: roundHeight
+    layers[numLayers - i - 1] = {
+      ratio, width: roundWidth, height: roundHeight,
       pixels: readPixels(context, 0, 0, roundWidth, roundHeight),
     }
 
     width /= 2
     height /= 2
+    ratio *= 2
   }
 
   return layers
